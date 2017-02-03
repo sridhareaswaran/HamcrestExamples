8d8231e8a9de53bd7158aa67895a926f570ab10d
Sridhar Easwaran <sridhar.1788@gmail.com>
2017-02-03 05:22:14 -0500

    Iterables

--- src/test/java/org/sri/hamcrest/iterables.java
+++ src/test/java/org/sri/hamcrest/iterables.java
@@ -3,11 +3,17 @@ package org.sri.hamcrest;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.core.Every.everyItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -16,9 +22,56 @@ import static org.junit.Assert.assertThat;
 public class iterables {
 
     @Test
-    public void hamcrest_core_every () {
+    public void hamcrest_core_every() {
         List<Integer> ages = Lists.newArrayList(21, 25, 30, 18);
         assertThat(ages, everyItem(greaterThanOrEqualTo(18)));
         assertThat(ages, everyItem(instanceOf(Integer.class)));
+    }
+
+    @Test
+    public void hamcrest_core_hasItems() {
+
+        List<String> regionalBreweries = Lists.newArrayList(
+                "Capital Brewery",
+                "City Brewing Company ",
+                "Jacob Leinenkugel Brewing Company",
+                "Lakefront Brewery, Inc.",
+                "New Glarus Brewing Company",
+                "Stevens Point Brewery");
+
+        assertThat(regionalBreweries, hasItems(
+                "Lakefront Brewery, Inc.",
+                "Capital Brewery",
+                "Jacob Leinenkugel Brewing Company",
+                "Stevens Point Brewery"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void hamcrest_core_hasItems_matchers() {
+
+        List<String> regionalBreweries = Lists.newArrayList(
+                "Capital Brewery",
+                "City Brewing Company ",
+                "Jacob Leinenkugel Brewing Company",
+                "Lakefront Brewery",
+                "New Glarus Brewing Company",
+                "Stevens Point Brewery");
+
+        assertThat(regionalBreweries, hasItems(
+                containsString("Brew"),
+                endsWith("y")));
+    }
+
+    @Test
+    public void hamcrest_core_contains() {
+        /** Order of items are also checked */
+        assertThat(Arrays.asList("foo", "bar"), contains("foo", "bar"));
+    }
+
+    @Test
+    public void hamcrest_core_containsInAnyOrder() {
+        /** Order of items doesn't matter, given all items are present */
+        assertThat(Arrays.asList("foo", "bar"), containsInAnyOrder("bar", "foo"));
     }
 }
